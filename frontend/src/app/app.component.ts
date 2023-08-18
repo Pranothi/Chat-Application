09a1e76569ea46d2e760c306d7c8eb8fbc8b0eb3
Pranothi <pranothi09@gmail.com>
2023-08-18 18:18:37 +0530

    Added images and Services

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChatService } from './services/chat/chat.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,79 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'frontend';
+  roomId:string = '';
+  messageText:string = '';
+  messageArray:{user:string, message:string}[]=[];
+
+  userList = [
+    {
+      id:1,
+      name: 'Abigail',
+      phone: '2003218888',
+      image: 'assets/user/dp.jpg',
+      roomId:{
+        2: 'room-1',
+        3: 'room-2',
+        4: 'room-3',
+        5: 'room-4'
+      }
+    },
+    {
+      id:2,
+      name: 'Blake',
+      phone: '3016219999',
+      image: 'assets/user/dp1.jpg',
+      roomId:{
+        1: 'room-1',
+        3: 'room-5',
+        4: 'room-6',
+        5: 'room-7'
+      }
+    },
+    {
+      id:3,
+      name: 'Joyel',
+      phone: '3251438742',
+      image: 'assets/user/dp2.jpg',
+      roomId:{
+        1: 'room-2',
+        2: 'room-5',
+        4: 'room-8',
+        5: 'room-9'
+      }
+    },
+    {
+      id:4,
+      name: 'Gloria',
+      phone: '2003218888',
+      image: 'assets/user/dp3.jpg',
+      roomId:{
+        1: 'room-3',
+        2: 'room-6',
+        3: 'room-8',
+        5: 'room-10'
+      }
+    },
+    {
+      id:5,
+      name: 'Clair',
+      phone: '8993526783',
+      image: 'assets/user/dp4.jpg',
+      roomId:{
+        1: 'room-4',
+        2: 'room-7',
+        3: 'room-9',
+        4: 'room-10'
+      }
+    },
+  ]
+
+  constructor(private chatservice:ChatService){
+    // chatservice.getMessage().subscribe( next : (data: {user:string, message:string}))
+  }
+
+  ngOnInit(){}
+
 }
